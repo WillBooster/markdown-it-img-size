d2cb33e36965a9316a1dc0ce091fa608c51a0326
Ryohei Takasawa <henshinfukanou@gmail.com>
2019-05-23 21:26:12 +0900

    WIP: Add logic to replace using regular expression

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,31 @@ import MarkdownIt from 'markdown-it';
 
 const index = (md: MarkdownIt, options: any) => {
   function convertMarkdownToHtml(src: any) {
-    // TODO: Add the logic here
-    return src;
+    if (src[0] === '!') {
+      const pattern = /([a-zA-Z0-9_\.\/\-]+)/g;
+      const matches = src.match(pattern);
+      const line: string =
+        '<p><img src="' +
+        matches[1] +
+        '" alt="' +
+        matches[0] +
+        '" width="' +
+        matches[2] +
+        '" height="' +
+        matches[3] +
+        '" /></p>';
+      console.log(line);
+      return line;
+    } else {
+      return src;
+    }
   }
 
   function replaceImgItSize(state: any, silent: any) {
     state.src = convertMarkdownToHtml(state.src);
   }
 
-  md.inline.ruler.before('image', 'image', replaceImgItSize);
+  md.inline.ruler.before('emphasis', 'image', replaceImgItSize);
 };
 
 export = index;
