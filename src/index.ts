24213ceec88901444c2a7e7a4116f9cd319fa3ae
Ryohei Takasawa <henshinfukanou@gmail.com>
2019-05-29 07:12:32 +0900

    fix: delete unnecessary return statements

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,10 @@ const index = (md: MarkdownIt, options: any) => {
           return `<img src="${matches[1]}" alt="${matches[0]}" width="${matches[2]}" />`;
         } else if (src.indexOf(`:${matches[2]}`) != -1) {
           return `<img src="${matches[1]}" alt="${matches[0]}" height="${matches[2]}" />`;
-        } else {
-          return src;
         }
       } else if (matches.length == 4) {
         return `<img src="${matches[1]}" alt="${matches[0]}" width="${matches[2]}" height="${matches[3]}" />`;
       }
-      return src;
     }
     return src;
   }
