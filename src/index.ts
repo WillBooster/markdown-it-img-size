de961d9f1d544e69b60fc423c023cfb933a41912
Ryohei Takasawa <henshinfukanou@gmail.com>
2019-05-28 19:54:05 +0900

    fix: use template strings

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,9 @@ const index = (md: MarkdownIt, options: any) => {
     if (src[0] === '!') {
       const pattern = /([a-zA-Z0-9_\.\/\-]+)/g;
       const matches = src.match(pattern);
-      const line: string =
-        '<img src="' +
-        matches[1] +
-        '" alt="' +
-        matches[0] +
-        '" width="' +
-        matches[2] +
-        '" height="' +
-        matches[3] +
-        '" /></p>';
+      const line: string = `<img src="${matches[1]}" alt="${matches[0]}" width="${matches[2]}" height="${
+        matches[3]
+      }" /></p>`;
       console.log(line);
       return line;
     } else {
