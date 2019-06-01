390210e55693293164d56fcf7dd29d978a26e45c
Sakamoto, Kazunori <exkazuu@gmail.com>
2019-06-01 10:54:35 +0900

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,25 @@
 import MarkdownIt from 'markdown-it';
+import StateCore = require('markdown-it/lib/rules_core/state_core');
 
 const index = (md: MarkdownIt, options: any) => {
-  function getImgElements(str: any) {
-    const pattern = /([a-zA-Z0-9_\.\/\-\%]+)/g;
-    const matches = str.match(pattern);
-    const img: string | null = null;
-    const alt: string | null = null;
-    const width: string | null = null;
-    const height: string | null = null;
-    return {
-      img: matches[0],
-      alt: matches[1],
-      width: matches[2],
-      height: matches[3],
-    };
-  }
+  const imgRegex = /!\s*\[\s*([^\]]+)\s*\]\s*\(\s*([^\)]+)\s*\)/g;
+  const fileAndSizeRegex = /([^=\s]+)\s*=([^:]*):([^:]*)/;
 
-  function convertMarkdownToHtml(src: any) {
-    const properPattern = /\s*\[\s*([a-zA-Z0-9_\.\-]+)\s*\]\s*\(\s*([a-zA-Z0-9_\.\-\%\=\:\s]+)\s*\)\s*/g;
-    const str = src.match(properPattern);
-    console.log(getImgElements(str[0]));
-    if (str[0] != null) {
-      const pattern = /([a-zA-Z0-9_\.\/\-\%]+)/g;
-      const matches = src.match(pattern);
-      if (matches.length == 3) {
-        if (src.indexOf(`${matches[2]}:`) != -1) {
-          return `<img src="${matches[1]}" alt="${matches[0]}" width="${matches[2]}" />`;
-        } else if (src.indexOf(`:${matches[2]}`) != -1) {
-          return `<img src="${matches[1]}" alt="${matches[0]}" height="${matches[2]}" />`;
-        }
-      } else if (matches.length == 4) {
-        return `<img src="${matches[1]}" alt="${matches[0]}" width="${matches[2]}" height="${matches[3]}" />`;
+  function convertMarkdownToHtml(src: string) {
+    return src.replace(imgRegex, (substr: string, alt: string, img: string) => {
+      const fileAndSizeMatch = img.match(fileAndSizeRegex);
+      if (!fileAndSizeMatch) {
+        return `<img src="${img}" alt="${alt}" />`;
       }
-    }
-    return src;
+
+      const [, file, width, height] = fileAndSizeMatch;
+      const widthAttr = width ? ` width="${width}"` : '';
+      const heightAttr = height ? ` height="${height}"` : '';
+      return `<img src="${file}" alt="${alt}"${widthAttr}${heightAttr} />`;
+    });
   }
 
-  function replaceImgItSize(state: any, silent: any) {
+  function replaceImgItSize(state: StateCore, silent: any) {
     state.src = convertMarkdownToHtml(state.src);
   }
 
